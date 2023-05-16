7f9c9fdaa0866edc51eac860be6247044ba97e6c
IrouKaizen <122926735+IrouKaizen@users.noreply.github.com>
2023-05-16 19:30:13 +0000

    Add files via upload
    My first Hello guys in Dart
    1- ctrl + maj + p
    2- write dart
    3- click on "console application"
    4- Give name to the project dart
    5- And you'll find it in "bin"

---
+++ Dart/first/example/first_example.dart
@@ -0,0 +1,6 @@
+import 'package:first/first.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ Dart/first/lib/first.dart
@@ -0,0 +1,13 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library first;
+
+export 'src/first_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
+
+void main(List<String> args) {
+  print('Hello guys');
+}
+ 
